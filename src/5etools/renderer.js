f2a89fdb31f46f3f452816ea2399de5fd2963449
Brian Matthews <borg286@gmail.com>
2021-05-30 19:24:51 -0700

    Reduction in contents of 5etools files

--- src/5etools/renderer.js
+++ src/5etools/renderer.js
@@ -1,3 +1,6 @@
+//TODO I suspect the contents of this file is irrelevant for the purposes of simply pulling rolls from 5e tools and pushing them to other VTTs.
+// This file ws copied from roll20, which was a reciever of rolls, but we are targeting making a publisher of rolls.
+
 
 class Roll20Displayer {
     sendMessage(request, title, html, character, whisper, play_sound, source, attributes, description, attack_rolls, roll_info, damage_rolls, total_damages, open) {
